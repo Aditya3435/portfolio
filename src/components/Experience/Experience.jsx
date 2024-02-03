ef8918558ff3672e1f51f7c4a7434073f6ecd5d2
Aditya Maurya <21bce056@nith.ac.in>
2024-02-03 13:38:39 +0530

    changed cp profiles design

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,12 +1,13 @@
 import React, { useRef } from 'react'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';import 'react-vertical-timeline-component/style.min.css'
+import StarIcon from '@mui/icons-material/Star';
 import { useInView } from 'framer-motion';
 function Experience() {
     const ref = useRef(null); // 1263 x 563
     const view = useInView(ref);
     return (
-        <div className='flex flex-col items-center justify-center gap-8 md:w-full w-11/12 h-screen max-sm:mt-10' ref={ref}>
+        <div className='flex flex-col items-center justify-center gap-8 md:w-full w-11/12 max-sm:mt-10' ref={ref}>
             <h1 className='text-5xl font-bold mb-10'>Experience</h1>
             <VerticalTimeline lineColor='var(--foreground-rgb)'>
                 <VerticalTimelineElement
@@ -19,11 +20,19 @@ function Experience() {
                     iconStyle={{ background: 'var(--foreground-rgb)', color: 'var(--background-end-rgb)', borderColor: 'var(--background-end-rgb)', borderWidth: '2px' }}
                     icon={<BusinessCenterIcon/>}
                 >
-                    <div className="vertical-timeline-element-title font-bold mt-2 mb-2">Coordinator</div>
-                    <a href='https://github.com/pixonoids' className='vertical-timeline-element-subtitle text-teal' target='_blank'>Pixonoids</a>
-                    <p>
-                        Collaborated with the team to develop college fest websites and mentored fellow students in web development.
+                    <div className="vertical-timeline-element-title font-bold mt-2 mb-2  text-teal">Web Developer</div>
+                    <a href='https://github.com/pixonoids' className='vertical-timeline-element-subtitle' target='_blank'>Pixonoids</a>
+                    <p className=''>
+                        1. Collaborated with the team to develop college fest websites using the MERN stack.
                     </p>
+                    <p>
+                        2. Developed farewell website using HTML, CSS and VanillaJS, for passing seniors. <a href='https://pixonoids.github.io/farewell-2022/' className='text-teal' target='_blank'>(Farewell 2k22 site) </a>
+                    </p>
+                    <p>
+                        3. Led cross-functional teams of two departments, Graphics Design and Animation, to integrate visually
+                        appealing elements on the website.
+                    </p>
+                    
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     visible={view}
@@ -39,12 +48,17 @@ function Experience() {
                     <div className='mt-2'>
                         1. Solved 700+ problems on <a href='https://leetcode.com/aditya3435/' target='_blank' className='text-teal'>Leetcode</a>
                         <br />
-                        2. 3 star coder on <a href='https://codechef.com/users/aditya3435' target='_blank' className='text-teal'>Codechef</a>
+                        2. 3 star coder on <a href='https://codechef.com/users/aditya3435' target='_blank' className='text-teal'>Codechef</a> (max. rating 1701)
                         <br /> 
-                        3. Pupil at <a href='https://codeforces.com/profile/aditya3435' target='_blank' className='text-teal'>Codeforces</a> (rating 1223)
+                        3. Pupil at <a href='https://codeforces.com/profile/aditya3435' target='_blank' className='text-teal'>Codeforces</a> (max. rating 1338)
                     </div>
                 </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    visible={view}
+                    iconStyle={{ background: 'var(--foreground-rgb)', color: 'var(--background-end-rgb)', borderColor: 'var(--background-end-rgb)', borderWidth: '2px' }}
 
+                    icon={<StarIcon />}
+                />
             </VerticalTimeline>
         </div>
     )
